949fe0da36576baed82c26364977c4e8b81182fa
Romain Forlot <romain.forlot@iot.bzh>
2018-07-26 14:41:01 +0200

    Fix: wrong legacy function signature
    Old function had not the third argument.
    
    Change-Id: Ifdcf341c0339aff80f9a6741f63ae0e7f8fae682
    Signed-off-by: Romain Forlot <romain.forlot@iot.bzh>

--- ctl-lib/ctl-config.h
+++ ctl-lib/ctl-config.h
@@ -106,7 +106,7 @@ extern int CtlConfigExec(AFB_ApiT apiHandle, CtlConfigT *ctlConfig) ;
 extern CtlConfigT *CtlLoadMetaDataJson(AFB_ApiT apiHandle,json_object *ctlConfigJ, const char *prefix) ;
 extern CtlConfigT *CtlLoadMetaDataUsingPrefix(AFB_ApiT apiHandle,const char* filepath, const char *prefix) ;
 extern int CtlLoadSections(AFB_ApiT apiHandle, CtlConfigT *ctlHandle, CtlSectionT *sections);
-#define CtlLoadMetaData(api, filepath, prefix) CtlLoadMetaDataUsingPrefix(api, filepath, NULL)
+#define CtlLoadMetaData(api, filepath) CtlLoadMetaDataUsingPrefix(api, filepath, NULL)
 
 // ctl-event.c
 extern int EventConfig(AFB_ApiT apihandle, CtlSectionT *section, json_object *actionsJ);
