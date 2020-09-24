bd967d8aa922ed66214e61266c05103f01405ce8
José Bollo <jose.bollo@iot.bzh>
2020-09-24 16:00:53 +0200

    Fix wrong success
    Some calls to AFB_ReqFail were replaced by afb_req_success
    in the commit eaf644db7106061481710adb75023122ce4bc150.

--- ctl-lib/ctl-lua.c
+++ ctl-lib/ctl-lua.c
@@ -406,7 +406,7 @@ static int LuaAfbFail(lua_State* luaState) {
     json_object *responseJ = LuaPopArgs(source, luaState, LUA_FIRST_ARG + 1);
     if (responseJ == JSON_ERROR) return 1;
 
-    afb_req_success(source->request, json_object_new_string(source->uid), json_object_get_string(responseJ));
+    afb_req_fail(source->request, source->uid, json_object_get_string(responseJ));
 
     json_object_put(responseJ);
     return 0;
@@ -978,7 +978,7 @@ static void LuaDoAction(LuaDoActionT action, afb_req_t request) {
 
         default:
             AFB_API_ERROR(source->api, "LUA-DOSCRIPT-ACTION unknown query=%s", json_object_to_json_string(queryJ));
-            afb_req_success(request, json_object_new_string("LUA:ERROR"), lua_tostring(luaState, -1));
+            afb_req_fail(request, "LUA:ERROR", lua_tostring(luaState, -1));
             return;
     }
 
@@ -986,7 +986,7 @@ static void LuaDoAction(LuaDoActionT action, afb_req_t request) {
         err = lua_pcall(luaState, count + 1, 0, 0);
     if (err) {
         AFB_API_ERROR(source->api, "LUA-DO-EXEC:FAIL query=%s err=%s", json_object_to_json_string(queryJ), lua_tostring(luaState, -1));
-        afb_req_success(request, json_object_new_string("LUA:ERROR"), lua_tostring(luaState, -1));
+        afb_req_fail(request, "LUA:ERROR", lua_tostring(luaState, -1));
         return;
     }
     return;
